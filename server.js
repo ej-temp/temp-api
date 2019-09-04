6053abe0f1c520fc89accd8368f6f78686f9528d
Jackmc <jackmc1122@gmail.com>
2019-09-04 13:22:14 -0700

    Send subscribe upon starting the server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
+const { subscribe } = require('../lib/services/network');
 
 const app = require('./lib/app');
 
@@ -8,4 +9,5 @@ const PORT = process.env.PORT || 7890;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
+  subscribe();
 });
