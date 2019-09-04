719d9609f94075d0b6652503318f364d915e224f
Jackmc <jackmc1122@gmail.com>
2019-09-04 09:59:40 -0700

    Add subscribe function and test

---
+++ test/network.test.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const { subscribe } = require('../lib/services/network');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('subscibes to the network', () => {
+    return subscribe()
+      .then(res => {
+        console.log(res);
+      });
+  });
+});
