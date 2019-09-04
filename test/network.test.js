697498164f06d7b969be9520a3508b06841adaba
Jackmc <jackmc1122@gmail.com>
2019-09-04 13:15:25 -0700

    Return list of most recent temps

--- test/network.test.js
+++ test/network.test.js
@@ -6,6 +6,7 @@ const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const { subscribe } = require('../lib/services/network');
 const Location = require('../lib/models/Location');
+const Temperature = require('../lib/models/Temperature');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -69,22 +70,77 @@ describe('app routes', () => {
         expect(res.body).toEqual({
           _id: jupiter._id.toString(),
           name: 'Jupiter',
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
           __v: 0
         });
       });
   });
 
   it('Adds a temperature', async() => {
-    const portland = await Location.create({ name: 'Portland' });
+    const cities = await Location.create([
+      { name: 'Portland' },
+      { name: 'Cleveland' },
+      { name: 'Dayton' },
+      { name: 'Seattle' },
+    ]);
+
+    await Temperature.create([
+      { locationId: cities[1]._id, temp: 80 },
+      { locationId: cities[2]._id, temp: 70 },
+      { locationId: cities[1]._id, temp: 50 },
+      { locationId: cities[3]._id, temp: 60 },
+      { locationId: cities[1]._id, temp: 83 },
+      { locationId: cities[3]._id, temp: 10 },
+      { locationId: cities[1]._id, temp: 20 },
+      { locationId: cities[2]._id, temp: 60 },
+      { locationId: cities[1]._id, temp: 54 },
+    ]);
+
     return request(app)
-      .post(`/temp/${portland._id}`)
+      .post(`/temp/${cities[0]._id}`)
       .send({ temperature: 87.5 })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          locationId: portland._id.toString(),
+          locationId: cities[0]._id.toString(),
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
           temp: 87.5,
           __v: 0
+        });
+      });
+  });
+
+  it('returns the list of locations with their most recent temp', async() => {
+    const cities = await Location.create([
+      { name: 'Portland' },
+      { name: 'Cleveland' },
+      { name: 'Dayton' },
+      { name: 'Seattle' },
+    ]);
+
+    await Temperature.create([
+      { locationId: cities[1]._id, temp: 80 },
+      { locationId: cities[2]._id, temp: 70 },
+      { locationId: cities[1]._id, temp: 50 },
+      { locationId: cities[3]._id, temp: 60 },
+      { locationId: cities[0]._id, temp: 83 },
+      { locationId: cities[3]._id, temp: 10 },
+      { locationId: cities[1]._id, temp: 20 },
+      { locationId: cities[2]._id, temp: 60 },
+      { locationId: cities[1]._id, temp: 54 },
+    ]);
+
+    return request(app)
+      .get('/api/v1/temperatures')
+      .then(res => {
+        expect(res.body).toHaveLength(4);
+        expect(res.body[0]).toEqual({
+          _id: expect.any(String),
+          temp: expect.any(Number),
+          name: expect.any(String),
+          createdAt: expect.any(String)
         });
       });
   });
